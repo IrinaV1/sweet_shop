325872cd28c9060928f971f52441b0ee3dbf1277
Irina <vyustirina@gmail.com>
2026-05-22 23:42:19 +0300

    add mob dessert-list

--- src/js/header.js
+++ src/js/header.js
@@ -1,5 +1,4 @@
-export const btn = document.querySelector('.header-burger-btn');
-
-btn.addEventListener('click', () => {
-  btn.classList.toggle('is-open');
-});
+// export const btn = document.querySelector('.header-burger-btn');
+// btn.addEventListener('click', () => {
+//   btn.classList.toggle('is-open');
+// });
