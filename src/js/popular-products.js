6de8ea6a37c69ba90da6dda5926394213d4b7f17
Irina <vyustirina@gmail.com>
2026-05-27 19:25:59 +0300

    add swiper

--- src/js/popular-products.js
+++ src/js/popular-products.js
@@ -1,13 +1,7 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 import { getDesserts } from './services/api/api';
-const swiper = new Swiper('.mySwiper', {
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true,
-  },
-});
+
 const dessertContainer = document.querySelector('.popular-product-list');
 
 document.addEventListener('DOMContentLoaded', popularProducts);
@@ -22,6 +16,7 @@ async function popularProducts() {
     console.log(data.desserts);
 
     renderDesserts(data.desserts);
+    initSwiper();
   } catch (error) {
     console.log(error.message);
   }
@@ -30,16 +25,16 @@ function renderDesserts(arr) {
   const markup = arr
     .map(
       ({ name, _id, image, price, category, description }) => `
-  <li class="dessert-list-item" data-id="${_id}">
-        <div class="dessert-list-wrapper">
-          <img class="dessert-list-img" src="${image}" alt="${name}">
+  <li class="popular-product-item swiper-slide" data-id="${_id}">
+        <div class="popular-product-wrapper">
+          <img class="popular-product-img" src="${image}" alt="${name}">
         </div>
-        <p class="dessert-list-category">${category.name}</p>
-        <h3 class="dessert-list-product">${name}</h3>
-        <p class="dessert-list-description">${description}</p>
-        <span class="dessert-list-price">${price} грн</span>
-        <button class="dessert-list-btn" type="button" aria-label="dessert list">
-          <svg class="dessert-list-icon" width="24" height="24">
+        <p class="popular-product-category">${category.name}</p>
+        <h3 class="popular-product-product">${name}</h3>
+        <p class="popular-product-description">${description}</p>
+        <span class="popular-product-price">${price} грн</span>
+        <button class="popular-product-btn" type="button" aria-label="dessert list">
+          <svg class="popular-product-icon" width="24" height="24">
             <use href="./img/icons/sprite.svg#arrow_outward"></use>
           </svg>
         </button>
@@ -47,4 +42,32 @@ function renderDesserts(arr) {
     )
     .join('');
   dessertContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+function initSwiper() {
+  new Swiper('.popular-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 16,
+
+    navigation: {
+      nextEl: '.popular-next',
+      prevEl: '.popular-prev',
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+  });
 }
